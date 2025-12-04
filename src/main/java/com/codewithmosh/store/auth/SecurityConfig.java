2169532ebb83f734a518e6ddbe5952cc22732b41
Muhammad Taimoor <mtaimoor0167@gmail.com>
2025-12-04 19:10:53 +0500

    Modularizing security rules

--- src/main/java/com/codewithmosh/store/auth/SecurityConfig.java
+++ src/main/java/com/codewithmosh/store/auth/SecurityConfig.java
@@ -1,11 +1,10 @@
 package com.codewithmosh.store.auth;
 
 import com.codewithmosh.store.common.LoggingRequestFilter;
-import com.codewithmosh.store.users.Role;
+import com.codewithmosh.store.common.SecurityRules;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
@@ -22,6 +21,8 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import java.util.List;
+
 @Configuration
 @EnableWebSecurity
 @AllArgsConstructor
@@ -29,6 +30,7 @@ class SecurityConfig {
     private final UserDetailsService userDetailsService;
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
     private final LoggingRequestFilter loggingRequestFilter;
+    private final List<SecurityRules> featureSecurityRules;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -36,29 +38,10 @@ class SecurityConfig {
                 .sessionManagement(c -> c.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(
-                        c -> c
-                                .requestMatchers("/swagger-ui/**").permitAll()
-                                .requestMatchers("/swagger-ui.html").permitAll()
-                                .requestMatchers("/v3/api-docs/**").permitAll()
-
-                                .requestMatchers("/carts/**").permitAll()
-
-                                .requestMatchers("/admin/**").hasRole(Role.ADMIN.name())
-
-                                .requestMatchers(HttpMethod.POST, "/users").permitAll()
-
-                                .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
-                                .requestMatchers(HttpMethod.POST, "/auth/validate").permitAll()
-                                .requestMatchers(HttpMethod.POST, "/auth/refresh").permitAll()
-
-                                .requestMatchers(HttpMethod.POST, "/checkout/webhook").permitAll()
-
-                                .requestMatchers(HttpMethod.GET, "/products/**").permitAll()
-                                .requestMatchers(HttpMethod.POST, "/products/**").hasRole(Role.ADMIN.name())
-                                .requestMatchers(HttpMethod.PUT, "/products/**").hasRole(Role.ADMIN.name())
-                                .requestMatchers(HttpMethod.DELETE, "/products/**").hasRole(Role.ADMIN.name())
-
-                                .anyRequest().authenticated()
+                        c -> {
+                            featureSecurityRules.forEach(r -> r.configure(c));
+                            c.anyRequest().authenticated();
+                        }
                 )
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .addFilterAfter(loggingRequestFilter, JwtAuthenticationFilter.class)
